5343e2b77d6fdc89ddb0d9b2382077394a198bc8
Seba <sebastian.pitra10@gmail.com>
2023-03-09 15:51:49 -0300

    fix: connection and disconnection with database

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,10 +1,10 @@
 import Cabin from "@/models/Cabin";
-import { connect } from "@/db/database";
+import { connectMongoDb } from "@/db/database";
 
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await connect();
-  const cabins = await Cabin.find();
-  res.json(cabins);
+  await connectMongoDb();
+  // const cabins = await Cabin.find();
+  res.json("golaa");
 };
